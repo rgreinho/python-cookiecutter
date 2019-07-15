5e9597e09b855d47e7ae5737ba832bf16aaa7b63
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-07-15 18:54:38 -0500

    Update the cookiecutter (#30)
    * Update dependencies
    * Replace Makefile with nox+invoke
    * Add top level invoke tasks
      * Replaces the `Makefile` with an `invoke` task file.

--- {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
@@ -1,5 +1,13 @@
+import pytest
+
+
 class Test{{ cookiecutter.project_name |capitalize }}():
     """Tests for `{{ cookiecutter.project_name }}` module."""
 
     def test_one(self):
         assert True
+
+
+@pytest.mark.integrations
+def test_two():
+    assert True
